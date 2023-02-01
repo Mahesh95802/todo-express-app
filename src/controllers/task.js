5988dd6ad66bdfa9c16f5b1b7611ac4f2047d670
Maheshwaran K <maheshwarankirupa@gmail.com>
2023-02-02 00:05:51 +0530

    feat: add error handing in POST /task

--- src/controllers/task.js
+++ src/controllers/task.js
@@ -1,18 +1,35 @@
+const Joi = require('joi')
 const taskService = require('../services/task')
+const HTTPError = require('../utils/errors/httpError')
 
 const getTasks = async (req, res) => {
-    console.log("/tasks controller is called")
-    res.send(await taskService.getTasks())
+    console.log("GET /tasks controller is called")
+    const response = await taskService.getTasks()
+    res.send(response)
 }
 
 const getTask = async (req, res) => {
-    console.log("/tasks/:id controller is called")
+    console.log("GET /tasks/:id controller is called")
     res.send(await taskService.getTask(req.params.id))
 }
 
 const postTask = async (req, res) => {
     console.log("POST /tasks/ controller is called")
-    res.send(await taskService.postTask(req.body.name))
+    try{
+        console.log(req.body)
+        const postTaskSchema = Joi.object({
+            name: Joi.string().required()
+        })
+        const { error, value } = postTaskSchema.validate(req.body)
+        console.log(error, value)
+        if(error) throw new HTTPError(400, error.message)
+        res.send(await taskService.postTask(req.body.name))
+    } catch(err) {
+        if(err instanceof HTTPError) {
+            return res.status(err.code).send({ message: err.message })
+        }
+        res.status(500).send(err.message)
+    }
 }
 
 const completeTask = async (req, res) => {
